b0f1e13ecaa4d512e7d2e8eea4de7bc514e88944
Diego Argueta <diego.argueta@cloverhealth.com>
2016-11-03 17:41:58 -0700

    More PEP 484 typing.

--- temporal_sqlalchemy/session.py
+++ temporal_sqlalchemy/session.py
@@ -1,20 +1,21 @@
 import datetime
 import itertools
-import typing
 
 import sqlalchemy.event as event
 import sqlalchemy.orm as orm
 
-from temporal_sqlalchemy.bases import ClockedOption, Clocked
+from temporal_sqlalchemy.bases import ClockedOption
 
 
-def _temporal_models(session: orm.Session) -> typing.Iterable[Clocked]:
+def _temporal_models(session):  # type: orm.Session
+    # type: (...) -> Iterable[Clocked]
     for obj in session:
         if isinstance(getattr(obj, 'temporal_options', None), ClockedOption):
             yield obj
 
 
-def persist_history(session: orm.Session, flush_context, instances):
+def persist_history(session, flush_context, instances):
+    # type: (orm.Session, ...)
     if any(_temporal_models(session.deleted)):
         raise ValueError("Cannot delete temporal objects.")
 
@@ -23,6 +24,7 @@ def persist_history(session: orm.Session, flush_context, instances):
         obj.temporal_options.record_history(obj, session, correlate_timestamp)
 
 
-def temporal_session(session: orm.Session) -> orm.Session:
+def temporal_session(session):
+    # type: (orm.Session) -> orm.Session
     event.listen(session, 'before_flush', persist_history)
     return session

47b35a015dc818920855e9cbe7130bb0d7330b6d
David Flerlage <david.flerlage@cloverhealth.com>
2017-12-05 13:41:18 -0800

    do not nest

--- temporal_sqlalchemy/session.py
+++ temporal_sqlalchemy/session.py
@@ -1,5 +1,6 @@
 import datetime
 import itertools
+import random
 import typing
 import warnings
 
@@ -13,13 +14,13 @@ from temporal_sqlalchemy.metadata import (
 )
 
 
-CHANGESET_STACK = 'changeset_stack'
-IS_COMMITTING = 'is_committing'
-IS_VCLOCK_UNCHANGED = 'is_vclock_unchanged'
+CHANGESET_STACK = '__temporal_changeset_stack'
+IS_COMMITTING = '__temporal_is_committing'
+IS_VCLOCK_UNCHANGED = '__temporal_is_vclock_unchanged'
 
 
-def get_current_changeset(metadata):
-    return metadata[CHANGESET_STACK][-1]
+def get_current_changeset(session):
+    return session.info[CHANGESET_STACK][-1]
 
 
 def _temporal_models(session: orm.Session) -> typing.Iterable[Clocked]:
@@ -29,13 +30,14 @@ def _temporal_models(session: orm.Session) -> typing.Iterable[Clocked]:
 
 
 def _build_history(session, correlate_timestamp):
-    metadata = get_session_metadata(session)
-    changeset = get_current_changeset(metadata)
+    changeset = get_current_changeset(session)
     items = list(changeset.items())
     changeset.clear()
 
+    metadata = get_session_metadata(session)
     is_strict_mode = metadata.get('strict_mode', False)
-    is_vclock_unchanged = metadata.get(IS_VCLOCK_UNCHANGED, False)
+
+    is_vclock_unchanged = session.info.get(IS_VCLOCK_UNCHANGED, False)
     if items and is_strict_mode:
         assert not is_vclock_unchanged, \
             'commit() has triggered for a changed temporalized property without a clock tick'
@@ -48,11 +50,13 @@ def persist_history(session: orm.Session, flush_context, instances):
     if any(_temporal_models(session.deleted)):
         raise ValueError("Cannot delete temporal objects.")
 
+    # its possible the temporal session was initialized after the transaction has started
+    _initialize_metadata(session)
+
     correlate_timestamp = datetime.datetime.now(tz=datetime.timezone.utc)
     changed_rows = _temporal_models(itertools.chain(session.dirty, session.new))
 
-    metadata = get_session_metadata(session)
-    changeset = get_current_changeset(metadata)
+    changeset = get_current_changeset(session)
     for obj in changed_rows:
         if obj.temporal_options.allow_persist_on_commit:
             new_changes, is_vclock_unchanged = obj.temporal_options.get_history(obj)
@@ -64,69 +68,89 @@ def persist_history(session: orm.Session, flush_context, instances):
                 old_changes = changeset[obj]
                 old_changes.update(new_changes)
 
-            metadata[IS_VCLOCK_UNCHANGED] = metadata[IS_VCLOCK_UNCHANGED] and is_vclock_unchanged
+            session.info[IS_VCLOCK_UNCHANGED] = session.info[IS_VCLOCK_UNCHANGED] and is_vclock_unchanged
         else:
             obj.temporal_options.record_history(obj, session, correlate_timestamp)
 
     # if this is the last flush, build all the history
-    if metadata[IS_COMMITTING]:
+    if session.info[IS_COMMITTING]:
         _build_history(session, correlate_timestamp)
 
-        metadata[IS_COMMITTING] = False
+        session.info[IS_COMMITTING] = False
 
 
 def enable_is_committing_flag(session):
-    metadata = get_session_metadata(session)
+    """before_commit happens before before_flush, and we need to make sure the history gets built
+    during the final one of these two events, so we need to use the gross IS_COMMITTING flag to
+    control this behavior"""
+    session.info[IS_COMMITTING] = True
 
-    metadata[IS_COMMITTING] = True
-
+    # if the session is clean, a final flush won't happen, so try to build the history now
     if session._is_clean():
         correlate_timestamp = datetime.datetime.now(tz=datetime.timezone.utc)
         _build_history(session, correlate_timestamp)
 
     # building the history can cause the session to be dirtied, which will in turn call another
     # flush(), so we want to check this before reseting
+    # if there are more changes, flush will build them itself
     if session._is_clean():
-        metadata[IS_COMMITTING] = False
+        session.info[IS_COMMITTING] = False
+
+
+def _get_transaction_stack_depth(transaction):
+    depth = 0
+
+    current = transaction
+    while current:
+        depth += 1
+        current = transaction.parent
+
+    return depth
+
+
+ARGH = {}
+
+
+def _initialize_metadata(session):
+    if CHANGESET_STACK not in session.info:
+        session.info[CHANGESET_STACK] = []
+
+    if IS_COMMITTING not in session.info:
+        session.info[IS_COMMITTING] = False
+
+    if IS_VCLOCK_UNCHANGED not in session.info:
+        session.info[IS_VCLOCK_UNCHANGED] = True
+
+    # sometimes temporalize a session after a transaction has already been open, so we need to
+    # backfill any missing stack entries
+    if len(session.info[CHANGESET_STACK]) == 0:
+        depth = _get_transaction_stack_depth(session.transaction)
+        for i in range(depth):
+            session.info[CHANGESET_STACK].append({})
 
 
 def start_transaction(session, transaction):
-    metadata = get_session_metadata(session)
+    _initialize_metadata(session)
 
-    metadata[CHANGESET_STACK].append({})
+    session.info[CHANGESET_STACK].append({})
+
+    if len(session.info[CHANGESET_STACK]) > 2:
+        pass #import pdb; pdb.set_trace()
 
 
 def end_transaction(session, transaction):
-    metadata = get_session_metadata(session)
+    # wrap in if statement for cases when the session is temporalized after a transaction has
+    # started, and then there are no actual temporal changes
+    if len(session.info[CHANGESET_STACK]):
+        session.info[CHANGESET_STACK].pop()
 
-    metadata[CHANGESET_STACK].pop()
-
-    # clear out bookkeeping fields if we're ending a top most transaction
+    # reset bookkeeping fields if we're ending a top most transaction
     if transaction.parent is None:
-        metadata[IS_VCLOCK_UNCHANGED] = True
+        session.info[IS_VCLOCK_UNCHANGED] = True
+        session.info[IS_COMMITTING] = False
 
-        if metadata[IS_COMMITTING]:
-            warnings.warn('IS_COMMITTING is still set to True, something is mismatched with temporal session lifecycle events')
-
-
-def _initialize_metadata(session, strict_mode):
-    # sqlalchemy does some weird memoizing / localizing the info dict to each session, so we have
-    # to copy the dictionary if we're just updating it. We now have additional metadata beyond
-    # strict_mode that can't be destroyed between function calls.
-    old_metadata = get_session_metadata(session)
-    temporal_metadata = old_metadata.copy() if old_metadata else {}
-    temporal_metadata['strict_mode'] = strict_mode
-
-    if CHANGESET_STACK not in temporal_metadata:
-        temporal_metadata[CHANGESET_STACK] = []
-
-    if IS_COMMITTING not in temporal_metadata:
-        temporal_metadata[IS_COMMITTING] = False
-
-    if IS_VCLOCK_UNCHANGED not in temporal_metadata:
-        temporal_metadata[IS_VCLOCK_UNCHANGED] = True
-
-    return temporal_metadata
+        # there should be no more changeset stacks at this point, otherwise there is a mismatch
+        assert len(session.info[CHANGESET_STACK]) == 0
 
 
 def temporal_session(session: typing.Union[orm.Session, orm.sessionmaker], strict_mode=False) -> orm.Session:
@@ -137,7 +161,9 @@ def temporal_session(session: typing.Union[orm.Session, orm.sessionmaker], stric
     :param strict_mode: if True, will raise exceptions when improper flush() calls are made (default is False)
     :return: temporalized SQLALchemy ORM session
     """
-    temporal_metadata = _initialize_metadata(session, strict_mode)
+    temporal_metadata = {
+        'strict_mode': strict_mode
+    }
 
     # defer listening to the flush hook until after we update the metadata
     install_flush_hook = not is_temporal_session(session)
